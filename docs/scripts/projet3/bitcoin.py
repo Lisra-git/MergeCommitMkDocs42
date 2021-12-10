c6d340262806e4b93af690c479e0b01cf46b41d9
bouillotvincent <vincent.bouillot.sa@gmail.com>
2021-12-10 16:45:26 +0100

    up

--- docs/scripts/projet3/bitcoin.py
+++ docs/scripts/projet3/bitcoin.py
@@ -15,12 +15,12 @@ def racine_approchee(y, p = p_easy, marge = 0):  # racine_approchee
     return x
     
 
-print(timeit.timeit("verification(6543210, 8371779, 15486869)", globals=globals()))
+# print(timeit.timeit("verification(6543210, 8371779, 15486869)", globals=globals()))
 
-print(timeit.timeit("racine(17, 101)", globals=globals(), number=5)/5)
-print(timeit.timeit("racine(8371779, p_mid)", globals=globals(), number=1)/1)
-print(timeit.timeit("racine(8371779, p_mid, 20)", globals=globals(), number=1)/1)
-print(timeit.timeit("racine(8371779, p_hard, 20)", globals=globals(), number=1)/1)
-print(racine_approchee(17, 101))
-print(racine_approchee(8371779, p_mid))
-print(racine_approchee(8371779, p_hard, 20))
+# print(timeit.timeit("racine_approchee(17, 101)", globals=globals(), number=5)/5)
+# print(timeit.timeit("racine_approchee(8371779, p_mid)", globals=globals(), number=1)/1)
+# print(timeit.timeit("racine_approchee(8371779, p_mid, 20)", globals=globals(), number=1)/1)
+# print(timeit.timeit("racine_approchee(8371779, p_hard, 20)", globals=globals(), number=1)/1)
+# print(racine_approchee(17, 101))
+# print(racine_approchee(8371779, p_mid))
+# print(racine_approchee(8371779, p_hard, 20))
