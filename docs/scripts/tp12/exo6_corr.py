0a1c8e040520d4ece9452090cc848d06f6ed4ded
bouillotvincent <vincent.bouillot.sa@gmail.com>
2022-02-11 11:48:14 +0100

    TP13 up

--- docs/scripts/tp12/exo6_corr.py
+++ docs/scripts/tp12/exo6_corr.py
@@ -1,25 +1,26 @@
 import tkinter as tk
 
 TOUR = 1
+COTE_CARRE = 130
 
 def tracer_croix(x_clic : int, y_clic : int) -> None:
-    canvas.create_line(x_clic, y_clic, x_clic + 130, y_clic + 130, width=5, fill='blue')
-    canvas.create_line(x_clic, y_clic + 130, x_clic + 130, y_clic, width=5, fill='blue')
+    canvas.create_line(x_clic, y_clic, x_clic + COTE_CARRE, y_clic + COTE_CARRE, width = 5, fill = 'blue')
+    canvas.create_line(x_clic, y_clic + COTE_CARRE, x_clic + COTE_CARRE, y_clic, width = 5, fill = 'blue')
 
 def tracer_cercle(x_clic : int, y_clic : int) -> None:
-    canvas.create_oval(x_clic, y_clic, x_clic + 130, y_clic + 130, width=5, outline='white')
+    canvas.create_oval(x_clic, y_clic, x_clic + COTE_CARRE, y_clic + COTE_CARRE, width = 5, outline = 'red')
 
 def tracer_damier():
-    for i in [0, 130, 260]:
-        for j in [0, 130, 260]:
-            canvas.create_rectangle(i, j, i + 130, j + 130, width=2, fill='white')
+    for i in [0, COTE_CARRE, 2*COTE_CARRE]:
+        for j in [0, COTE_CARRE, 2*COTE_CARRE]:
+            canvas.create_rectangle(i, j, i + COTE_CARRE, j + COTE_CARRE, width = 2, fill = 'white')
 
-def calculer_coordonnées(x, y):
-    return x // 130*130, y // 130*130
+def calculer_coordonnees(x, y):
+    return (x // COTE_CARRE) * COTE_CARRE, (y // COTE_CARRE) * COTE_CARRE
 
 def jouer(event):
     global TOUR
-    x, y = calculer_coordonnées(event.x, event.y)
+    x, y = calculer_coordonnees(event.x, event.y)
     if TOUR % 2:
         tracer_croix(x, y)
         TOUR += 1
@@ -28,22 +29,22 @@ def jouer(event):
         TOUR += 1
 
 
-largeur, hauteur = 390, 390
+largeur, hauteur = 3 * COTE_CARRE, 3 * COTE_CARRE
 
 fenetre = tk.Tk()
-fenetre.title('Une fenêtre !')
+fenetre.title('Morpion !')
 
-lab = tk.Label(fenetre, text='')
-lab.grid(row = 0, column = 0, padx=3, pady=3, columnspan = 2)
+lab = tk.Label(fenetre, text = '')
+lab.grid(row = 0, column = 0, padx = 3, pady = 3, columnspan = 2)
 
 btn1 = tk.Button(fenetre, text= 'Quit', command = fenetre.quit)
-btn1.grid(row = 2, column = 1, padx=3, pady=3, sticky = tk.E + tk.W)
+btn1.grid(row = 2, column = 1, padx = 3, pady = 3, sticky = tk.E + tk.W)
 
 btn2 = tk.Button(fenetre, text= 'Play again')#, command = clear)
-btn2.grid(row = 2, column = 0, padx=3, pady=3, sticky = tk.E + tk.W)
+btn2.grid(row = 2, column = 0, padx = 3, pady = 3, sticky = tk.E + tk.W)
 
 canvas = tk.Canvas(fenetre, width= largeur, height = hauteur, background = 'black')
-canvas.grid(row = 1, column = 0, padx=3, pady=3, columnspan = 2)
+canvas.grid(row = 1, column = 0, padx = 3, pady = 3, columnspan = 2)
 tracer_damier()
 
 
